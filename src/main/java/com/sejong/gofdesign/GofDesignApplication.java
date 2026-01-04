2be1b2fd15b0af688d5098d6d63c19adbf6446ce
PlusUltraCode <kkd06144@naver.com>
2026-01-04 11:09:09 +0900

    초기 설정

---
+++ src/main/java/com/sejong/gofdesign/GofDesignApplication.java
@@ -0,0 +1,13 @@
+package com.sejong.gofdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GofDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GofDesignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sejong/gofdesign/GofDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sejong.gofdesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GofDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
